e26a2ff269d5dab78ec1c71849d47b8713ad70b4
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-10 17:39:36 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	# helps render these methods to the INDEX.html.erb & SHOW.html.erb accordingly named after there instance methods
 	def index
 		@posts = Post.all
 	end
